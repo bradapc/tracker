aa6429c09c0d732d2aef29dbd64fb0db51707191
bradapc <brad.cocar@gmail.com>
2024-05-06 13:54:24 -0700

    Added register page functionality

--- app.py
+++ app.py
@@ -1,12 +1,48 @@
 from flask import Flask, render_template, request, redirect
 import sqlite3
-import bcrypt
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "You're in!"
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        #Retrieve username, password, and confirmation and handle non-input errors
+        form_username = request.form.get("username")
+        form_password = request.form.get("password")
+        form_confirmation = request.form.get("confirmation")
+        if not form_username:
+            errormsg = "Please enter a username to register."
+            return render_template("register.html", errormsg=errormsg)
+        if not form_password:
+            errormsg = "Please enter a password to register."
+            return render_template("register.html", errormsg=errormsg)
+        if form_password != form_confirmation:
+            errormsg = "Password does not match confirmation."
+            return render_template("register.html", errormsg=errormsg)
+        #Query database to see if username already exists
+        con = sqlite3.connect("tracker.db")
+        cur = con.cursor()
+        res = cur.execute("SELECT * FROM users WHERE username = ?", (form_username,))
+        username_res = res.fetchall()
+        cur.close()
+        if username_res:
+            errormsg = "Username already exists. Please enter a different username."
+            return render_template("register.html", errormsg=errormsg)
+        #If username does not exist, hash password and store it in database.
+        cur_writeuser = con.cursor()
+        bcrypt = Bcrypt(app)
+        hashed_password = bcrypt.generate_password_hash(form_password).decode('utf-8')
+        cur_writeuser.execute("INSERT INTO users (username, hash) VALUES(?, ?)", (form_username, hashed_password,))
+        con.commit()
+        con.close()
+        return redirect("/")
+    else:
+        return render_template("register.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -30,8 +66,5 @@ def login():
         else:
             errormsg = "Password is incorrect. Please try again."
             return render_template("login.html", errormsg=errormsg)
-        #password = str.encode(password_plaintext)
-        #salt = bcrypt.gensalt()
-        #hashed = bcrypt.hashpw(password, salt)
     else:
         return render_template("login.html")
