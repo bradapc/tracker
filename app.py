fbc89ce7533dd00e02c8c8b5485e2f87a71cedaa
bradapc <brad.cocar@gmail.com>
2024-05-06 13:08:09 -0700

    Updated login css, began database for users

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect
+import sqlite3
+import bcrypt
 
 app = Flask(__name__)
 
@@ -9,6 +11,21 @@ def index():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
+        #Get username from form and check against database
+        con = sqlite3.connect("tracker.db")
+        cur = con.cursor()
+        username = request.form.get("username")
+        res = cur.execute("SELECT * FROM users WHERE username = ?", (username,))
+        username_res = res.fetchall()
+        if not username_res:
+            errormsg = "Username could not be found. Please register if you do not have an account."
+            return render_template("login.html", errormsg=errormsg)
+        con.close()
+        #Encode password as UTF-8 then hash using bcrypt
+        password_plaintext = request.form.get("password")
+        password = str.encode(password_plaintext)
+        salt = bcrypt.gensalt()
+        hashed = bcrypt.hashpw(password, salt)
         return redirect("/")
     else:
         return render_template("login.html")
